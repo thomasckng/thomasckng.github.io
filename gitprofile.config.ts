a1059666dc2f38f539ea5a9235750a15eedaeaf1
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-07-03 17:02:34 +0800

    Update Google Analytics tracking ID in configuration

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -163,7 +163,7 @@ const CONFIG = {
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-0ZBPF5KPWR', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
